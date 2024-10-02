bb29c832b19f3b3adfc50e40b501fdb899d8ce67
kalkeesh <kalkeeshjami@gmail.com>
2024-10-02 19:31:17 +0530

    good code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-# import google.generativeai as genai
+import google.generativeai as genai
 import base64
 
 app = FastAPI()
@@ -15,22 +15,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# class PromptInput(BaseModel):
-#     prompt: str
+class PromptInput(BaseModel):
+    prompt: str
 
-# genai.configure(api_key="AIzaSyBh9Uc0f20IZtJe3N6xog2U9eCOgBWsK4s")
-# model = genai.GenerativeModel('gemini-1.5-flash')
+genai.configure(api_key="AIzaSyBh9Uc0f20IZtJe3N6xog2U9eCOgBWsK4s")
+model = genai.GenerativeModel('gemini-1.5-flash')
 
-# @app.post("/generate")
-# async def generate_text(input: PromptInput):
-#     response = model.generate_content(input.prompt)
-#     encode = base64.b64encode(response.text.encode()).decode("utf-8")  
-#     return JSONResponse({"text": encode})
+@app.post("/generate")
+async def generate_text(input: PromptInput):
+    response = model.generate_content(input.prompt)
+    encode = base64.b64encode(response.text.encode()).decode("utf-8")  
+    return JSONResponse({"text": encode})
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 @app.get('/')
 async def generate():
     return {'text': 'successful'}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
